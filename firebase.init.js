bbd865073a6dcc5c74c7a726030574c05add55de
mdrayhanislam <96991135+mdrayhanislam@users.noreply.github.com>
2022-04-18 16:13:24 +0600

    firebase init file screate create

---
+++ firebase.init.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import{ getAuth } from 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDA0PuiSer5ePOG0T4YDl_EhGK6nrNuicw",
+  authDomain: "assignment-10-495ba.firebaseapp.com",
+  projectId: "assignment-10-495ba",
+  storageBucket: "assignment-10-495ba.appspot.com",
+  messagingSenderId: "918620243573",
+  appId: "1:918620243573:web:a251d531551de7034bda3c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export default auth;
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -8,17 +8,22 @@ const Register = () => {
     }
     const handleRegister = event =>{
         event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value
+        // console.log(event.target.email.value)
+        // console.log(event.target.password.value)
     }
     return (
         <div className='register-form'>
-         <h1 style={{textAlign: 'center'}}>  Please Register </h1>
+         <h1 style={{textAlign:'center'}}>  Please Register </h1>
          <form onSubmit={handleRegister}>
              <input type="text" name='name' id='' placeholder='Your Name' />
-             <input type="email" name='email' id='' placeholder='Your Email' />
-             <input type="password" name='password' id='' placeholder='Your Password' />
+             <input type="email" name='email' id='' placeholder='Your Email' required/>
+             <input type="password" name='password' id='' placeholder='Your Password' required/>
              <input type="submit" value="Register" />
          </form>
-         <p style={{textAlign: 'center'}}>All ready have an account? <Link to="/login" className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}> Please Login </Link></p>
+         <p style={{textAlign:'center'}}>All ready have an account? <Link to="/login" className='text-danger pe-auto text-decoration-none' onClick={navigateLogin}> Please Login </Link></p>
 
         </div>
     );
